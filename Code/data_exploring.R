519ca5f74319abd782e67105988971ac8ce5332c
niklasmuenz <niklas.muuenz@stud.uni-bamberg.de>
2024-06-19 13:05:27 +0200

    Splits nicht beantwortet entfernen

--- Code/data_exploring.R
+++ Code/data_exploring.R
@@ -4,3 +4,16 @@ allb <- read_sav("Code/Daten/ZA5281_v1-0-1.sav")
 class(allb)
 str(allb)
 length(unique(allb$respid))
+
+allb$dg03
+str(allb$dg03)
+
+str(allb$splt21)
+
+Split_A <- allb[allb$splt21 == 1, ]
+
+equal <- sapply(Split_A, function(x) length(unique(x)) == 1)
+nicht_beantwortet_A <- names(which(equal))
+
+
+
