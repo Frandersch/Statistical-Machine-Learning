94f39bb3bf0215c06cbd2fcc96cf0a6dfb9658da
Frandersch <franzandersch10@gmail.com>
2024-06-19 13:09:41 +0200

    merge2

--- Code/data_exploring.R
+++ Code/data_exploring.R
@@ -4,11 +4,6 @@ allb <- read_sav("Code/Daten/ZA5281_v1-0-1.sav")
 class(allb)
 str(allb)
 length(unique(allb$respid))
-<<<<<<< HEAD
-all.equal(allb$splt21)
-
-#afjksadfkj
-=======
 
 allb$dg03
 str(allb$dg03)
@@ -22,4 +17,4 @@ nicht_beantwortet_A <- names(which(equal))
 
 
 
->>>>>>> 519ca5f74319abd782e67105988971ac8ce5332c
+
