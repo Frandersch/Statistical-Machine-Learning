adf68a71d163628943eac512e7c327e6ed6caf7e
niklasmuenz <niklas.muuenz@stud.uni-bamberg.de>
2024-06-19 13:44:54 +0200

    Splits bereinigt

--- Code/data_exploring.R
+++ Code/data_exploring.R
@@ -10,11 +10,25 @@ str(allb$dg03)
 
 str(allb$splt21)
 
+#Datensatz nach jedem Split aufgeteilt
 Split_A <- allb[allb$splt21 == 1, ]
+Split_B <- allb[allb$splt21 == 2, ]
+Split_C <- allb[allb$splt21 == 3, ]
 
-equal <- sapply(Split_A, function(x) length(unique(x)) == 1)
-nicht_beantwortet_A <- names(which(equal))
+#Spalten in denen alle Antworten gleich sind identifiziert
+equal_A <- names(which(sapply(Split_A, function(x) length(unique(x)) == 1)))
+equal_B <- names(which(sapply(Split_B, function(x) length(unique(x)) == 1)))
+equal_C <- names(which(sapply(Split_C, function(x) length(unique(x)) == 1)))
+
+#Spalten identifizieren die nicht durch alle Splits beantwortet wurden
+nicht_alle_Splits <- unique(c(equal_A, equal_B, equal_C))
+
+behalten <- setdiff(names(allb), nicht_alle_Splits)
+
+#Datensatz bereinigt um Spalten, die nicht von allen bantwortet wurden
+allb_red1 <- allb[, behalten]
 
 
 
-
+NA_Anteil <- sapply(allb_red1, function(x) sum(is.na(x))/length(x))
+summary(NA_Anteil)
