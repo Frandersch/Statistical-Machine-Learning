6ecdceab1efe8b6b2184d2871b57a6be39a0eac4
niklasmuenz <niklas.muuenz@stud.uni-bamberg.de>
2024-06-25 17:00:10 +0200

    Anpassungen Code und Präsi

--- Code/data_exploring.R
+++ Code/data_exploring.R
@@ -7,40 +7,66 @@ class(allb)
 str(allb)
 length(unique(allb$respid))
 
-#Datensatz nach jedem Split aufgeteilt
-Split_A <- allb[allb$splt21 == 1, ]
-Split_B <- allb[allb$splt21 == 2, ]
-Split_C <- allb[allb$splt21 == 3, ]
+########################Datenpräparation###################################
 
-#Spalten in denen alle Antworten gleich sind identifiziert
-equal_A <- names(which(sapply(Split_A, function(x) length(unique(x)) == 1)))
-equal_B <- names(which(sapply(Split_B, function(x) length(unique(x)) == 1)))
-equal_C <- names(which(sapply(Split_C, function(x) length(unique(x)) == 1)))
+Datenpräparation_Allbus <- function(Allbus) {
+  #Datensatz nach jedem Split aufgeteilt
+  Split_A <- allb[allb$splt21 == 1,]
+  Split_B <- allb[allb$splt21 == 2,]
+  Split_C <- allb[allb$splt21 == 3,]
+  
+  #Spalten in denen alle Antworten gleich sind identifiziert
+  equal_A <-
+    names(which(sapply(Split_A, function(x)
+      length(unique(
+        x
+      )) == 1)))
+  equal_B <-
+    names(which(sapply(Split_B, function(x)
+      length(unique(
+        x
+      )) == 1)))
+  equal_C <-
+    names(which(sapply(Split_C, function(x)
+      length(unique(
+        x
+      )) == 1)))
+  
+  #Spalten identifizieren die nicht durch alle Splits beantwortet wurden
+  nicht_alle_Splits <- unique(c(equal_A, equal_B, equal_C))
+  
+  behalten <- setdiff(names(allb), nicht_alle_Splits)
+  
+  #Datensatz bereinigt um Spalten, die nicht von allen bantwortet wurden
+  allb_red1 <- allb[, behalten]
+  
+  
+  #aussortieren von Spalten mit mehr als 5% NA
+  NA_Anteil <- sapply(allb_red1, function(x)
+    sum(is.na(x)) / length(x))
+  Threshold <- 0.05
+  allb_red2 <- subset(allb_red1, select = NA_Anteil < Threshold)
+  
+  #administrative Spalten entfernen
+  allb_red3 <- allb_red2[,-c(1:4)]
+  
+  #alle Observations mit NA entfernen
+  allb_red4 <- na.omit(allb_red3)
+  
+  #nur relevante Fälle behalten (-42 Datenfehler, -9 keine Angabe,
+  #18 Fruehere Dt. Ostgeb. und 95 Sonstiges Land entfernen) und faktorisieren
+  allb_red4 <- allb_red4[allb_red4$dg10 == 1 | allb_red4$dg10 == 2,]
+  allb_red4$dg10 <- factor(allb_red4$dg10)
+  
+  #letzte Datenbearbeitung ausgeben
+  allb_red4
+}
 
-#Spalten identifizieren die nicht durch alle Splits beantwortet wurden
-nicht_alle_Splits <- unique(c(equal_A, equal_B, equal_C))
-
-behalten <- setdiff(names(allb), nicht_alle_Splits)
-
-#Datensatz bereinigt um Spalten, die nicht von allen bantwortet wurden
-allb_red1 <- allb[, behalten]
+allb_prep <- Datenpräparation_Allbus(allb)
 
 
-#aussortieren von Spalten mit mehr als 5% NA
-NA_Anteil <- sapply(allb_red1, function(x) sum(is.na(x))/length(x))
-Threshold <- 0.05
-allb_red2 <- subset(allb_red1, select = NA_Anteil < Threshold)
+################################Auswertung###################################
 
-#administrative Spalten entfernen
-allb_red3 <- allb_red2[, - c(1:4)]
-
-#alle Observations mit NA entfernen
-allb_red4 <- na.omit(allb_red3)
-
-#nur relevante Fälle behalten (-42 Datenfehler, -9 keine Angabe,
-#18 Fruehere Dt. Ostgeb. und 95 Sonstiges Land entfernen) und faktorisieren
-allb_red4 <- allb_red4[allb_red4$dg10 == 1 | allb_red4$dg10 == 2, ]
-allb_red4$dg10 <- factor(allb_red4$dg10)
 
 #test und train Daten definieren
 set.seed(123)
