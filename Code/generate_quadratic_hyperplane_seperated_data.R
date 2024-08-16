5fb0dca9e65b876b1447bac2d9ee3b35ca5885ad
Frandersch <franzandersch10@gmail.com>
2024-08-16 14:09:47 +0200

    quadratische daten in n dimesnionen

--- Code/generate_quadratic_hyperplane_seperated_data.R
+++ Code/generate_quadratic_hyperplane_seperated_data.R
@@ -1,7 +1,7 @@
 #quadratic Hyperplane in 3 dimensional space
 #z=ax^2+by^2+c,a=b=0.5,c=0
-x <- rnorm(100,0,1)
-y <- rnorm(100,0,1)
+x <- rnorm(100,0,2)
+y <- rnorm(100,0,2)
 z <- 0.5*x^2+0.5*y^2
 df <- data.frame(x=x,y=y,z=z)
 library(rgl)
@@ -11,14 +11,27 @@ df2 <- data.frame(x=rep(0,100),
                   y=rep(0,100),
                   z=rep(0,100))
 for(i in 1:nrow(df2)){
-  df2[i,]<- unlist(df[i,])+(rnorm(1,3,1)*c(df[i,1],df[i,2],-1))
+  df2[i,]<- unlist(df[i,])+(rnorm(1,3,1)*c(0,0,1))
 }
 df3 <- data.frame(x=rep(0,100),
                      y=rep(0,100),
                      z=rep(0,100))
 for(i in 1:nrow(df3)){
-  df3[i,]<- unlist(df[i,])+(rnorm(1,-3,1)*c(df[i,1],df[i,2],-1))
+  df3[i,]<- unlist(df[i,])+(rnorm(1,-3,1)*c(0,0,1))
 }
 df_final <- rbind(df2,df3)
 df_final$group <- c(rep(1,100),rep(2,100))
 plot3d(x=df_final$x,y=df_final$y,z=df_final$z,col=df_final$group)
+
+#erweiterung in n dimensionen
+generate_dataset <- function(obs,variables,distance,jitter,coef_min=-100,coef_max=100,center=0,range=3){
+  coefficients <- runif(2*variables-1,coef_min,coef_max)
+  xses <- matrix(rnorm(obs,center,range),nrow=obs,ncol=1)
+ for(i in 2:(variables-1)){
+   xses <- cbind(xses,rnorm(obs,center,range))
+ }
+  combinations<- xses%*%coefficients[1:(variables-1)]
+  quadcombinations <- xses^2%*%coefficients[variables:(2*variables-2)]
+  z <- combinations+quadcombinations+rep(coefficients[2+variables-1],obs)+rnorm(obs,distance,jitter)
+  return(as.data.frame(cbind(xses,z)))
+}
